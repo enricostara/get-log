e8babf0f1fecf6ddb8891f5a6362cb50a7d7be96
Enrico Stara <enrico.stara@gmail.com>
2014-10-30 00:26:42 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//       get-log
+//
+//       Copyright 2014 Enrico Stara 'enrico.stara@gmail.com'
+//       Released under the BSD-2-Clause license
+//       https://github.com/enricostara/get-log
+
+module.exports = exports = require('./lib/get-log');
+
---
+++ test/test.js
@@ -0,0 +1,30 @@
+require('should');
+process.env['DEBUG'] = '*,-*logger_name4';
+
+describe('get-log', function () {
+    describe('#require(..)("logger_name")', function () {
+        it('should return a logger called "logger_name"  ', function () {
+            var logger = require('../index')('logger_name');
+            logger.getName().should.be.equal('logger_name');
+        })
+    });
+    describe('#require(..)("logger_name2") and PROJECT_NAME = "project_name"', function () {
+        it('should return a logger called "project_name:logger_name2"  ', function () {
+            require('../index').PROJECT_NAME = 'project_name';
+            var logger = require('../index')('logger_name2');
+            logger.getName().should.be.equal('project_name:logger_name2');
+        })
+    });
+    describe('#isDebugEnabled()', function () {
+        it('should return true ', function () {
+            var logger = require('../index')('logger_name3');
+            logger.isDebugEnabled().should.be.true;
+        })
+    });
+    describe('#isDebugEnabled()', function () {
+        it('should return false ', function () {
+            var logger = require('../index')('logger_name4');
+            logger.isDebugEnabled().should.be.false;
+        })
+    });
+});
