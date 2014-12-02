1f916b33901dfcf1390130bd47414013935238fa
Enrico Stara <enrico.stara@gmail.com>
2014-12-02 11:25:20 +0100

    Fix test

--- test/test.js
+++ test/test.js
@@ -1,31 +1,32 @@
 require('should');
 var namespace = '*,-*logger_name4';
 process.env['DEBUG'] = namespace;
-require('../index').enable(namespace);
+var getLogger = require('../lib/get-log');
+getLogger.enable(namespace);
 
 describe('get-log', function () {
     describe('#require(..)("logger_name")', function () {
         it('should return a logger called "logger_name"  ', function () {
-            var logger = require('../index')('logger_name');
+            var logger = getLogger('logger_name');
             logger.getName().should.be.equal('logger_name');
         })
     });
     describe('#require(..)("logger_name2") and PROJECT_NAME = "project_name"', function () {
         it('should return a logger called "project_name:logger_name2"  ', function () {
-            require('../index').PROJECT_NAME = 'project_name';
-            var logger = require('../index')('logger_name2');
+            getLogger.PROJECT_NAME = 'project_name';
+            var logger = getLogger('logger_name2');
             logger.getName().should.be.equal('project_name:logger_name2');
         })
     });
     describe('#isDebugEnabled()', function () {
         it('should return true ', function () {
-            var logger = require('../index')('logger_name3');
+            var logger = getLogger('logger_name3');
             logger.isDebugEnabled().should.be.true;
         })
     });
     describe('#isDebugEnabled()', function () {
         it('should return false ', function () {
-            var logger = require('../index')('logger_name4');
+            var logger = getLogger('logger_name4');
             logger.isDebugEnabled().should.be.false;
         })
     });
