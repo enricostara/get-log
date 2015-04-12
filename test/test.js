d033706006294c7c6f8aba90dd4834afcdc67970
Enrico Stara <enrico.stara@gmail.com>
2015-04-12 12:54:40 +0200

    Add the file output stream management

--- test/test.js
+++ test/test.js
@@ -1,6 +1,8 @@
 require('should');
 var namespace = '*,-*logger_name4';
 process.env['DEBUG'] = namespace;
+process.env['LOGGER_FILE'] = 'test';
+
 var getLogger = require('../lib/get-log');
 getLogger.enable(namespace);
 
@@ -9,13 +11,6 @@ describe('get-log', function () {
         it('should return a logger called "logger_name"  ', function () {
             var logger = getLogger('logger_name');
             logger.getName().should.be.equal('logger_name');
-        })
-    });
-    describe('#require(..)("logger_name2") and PROJECT_NAME = "project_name"', function () {
-        it('should return a logger called "project_name:logger_name2"  ', function () {
-            getLogger.PROJECT_NAME = 'project_name';
-            var logger = getLogger('logger_name2');
-            logger.getName().should.be.equal('project_name:logger_name2');
         })
     });
     describe('#isDebugEnabled()', function () {
@@ -28,6 +23,16 @@ describe('get-log', function () {
         it('should return false ', function () {
             var logger = getLogger('logger_name4');
             logger.isDebugEnabled().should.be.false;
+        })
+    });
+    describe('#debug()', function () {
+        it('should return false ', function () {
+            var logger = getLogger('logger_name5');
+            logger.debug('debug');
+            logger.info('info');
+            logger.warn('warn');
+            logger.error('error');
+            (require('fs').readdirSync('.').indexOf(getLogger.logFileName) > -1).should.be.true;
         })
     });
 });
